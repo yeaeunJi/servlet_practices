b00465f8c58dc1d152c7cbe5404193b9f1defc1d
user <user@host.docker.internal>
2021-03-09 15:15:57 +0900

    HelloServelt과 index.jsp, tag.jsp 추가

--- servlet-practices/helloweb3/src/main/java/com/saltlux/helloweb3/controller/HelloServlet.java
+++ servlet-practices/helloweb3/src/main/java/com/saltlux/helloweb3/controller/HelloServlet.java
@@ -1,6 +1,8 @@
 package com.saltlux.helloweb3.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +12,10 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().print("<h1>hello servlet</h1>");
+		// 보내는 텍스트 내용에 한글이 포함된 문자셋을 사용하라고 알려줘야 함
+		response.setContentType("text/html; charset=utf-8"); // reseponse 헤더에 인코딩 정보를 담음
+		PrintWriter pw = response.getWriter();  // getWrtier() : \0000 빈 개행 문자 뒤에 body가 생성
+		pw.print("<h1>안녕 :) !!!</h1>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ servlet-practices/helloweb3/src/main/webapp/tag.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>hello world</h1>
+	<h2>hello world</h2>
+	<h3>hello world</h3>
+	<h4>hello world</h4>
+	<h5>hello world</h5>
+	<h6>hello world</h6>
+
+	<table border="1" cellspacing="0" cellpadding="10">
+		<tr>
+			<th>번호</th>
+			<th>제목</th>
+			<th>작성자</th>
+		</tr>
+		<tr>
+			<td>01 </td>
+			<td>test</td>
+			<td>tester01</td>
+		</tr>
+	</table>
+</body>
+</html>
